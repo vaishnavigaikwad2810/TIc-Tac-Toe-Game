c12ab3363788485ded87a83f0c1b3465eb03c56b
Vaishnavi Sunil Gaikwad <vaishnavigaikwad2810@gmail.com>
2023-02-17 12:39:57 -0800

    Final Commit

---
+++ app/src/main/java/com/example/tictactoe/splash/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.tictactoe.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.example.tictactoe.MainActivity
+import com.example.tictactoe.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
